e1ca27b9e1fcf1bfb486a005ebd1fb6a41f4512e
Mutooro <98326730+Mutooro@users.noreply.github.com>
2023-02-19 16:44:31 +0300

    Add files via upload

---
+++ project/dashboad/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/home.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="cards">
+                    <div class="card">
+                        <div class="box">
+                            <h1>player 1</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="js/bootstrap.js"></script>
+</html>
---
+++ project/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link type = "text/css" rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+        <div class="logo">
+            <!-- <img src="/add_users/user.png"> -->
+        </div>
+        <div class="welcome">
+            <h1>Hey, welcome to the official page of Mak Sports System</h1>
+        </div>
+        <div>
+            <button class="btn" name="login" href="login.php">login</button>
+            <button class="btn" name="register" href="register.php">Signup</button>
+        </div>
+        <div>
+    </body>
+</html>
